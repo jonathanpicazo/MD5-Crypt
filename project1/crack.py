e087c24af36e2e4274d8245a73718e1b96babffc
jonathanpicazo <43653582+jonathanpicazo@users.noreply.github.com>
2021-01-12 22:23:38 -0800

    finished combo function

--- project1/crack.py
+++ project1/crack.py
@@ -1,7 +1,18 @@
 #team20:$1$4fTgjp6q$QhqtcnMK1gPnk0uN3CsyL/:16653:0:99999:7:::
 from itertools import combinations
+from itertools import permutations
 import hashlib
+import string
+
 #TODO: start combinations
+
+def combo():
+    possible = string.ascii_lowercase
+    #print(possible)
+    for i in range(1,6):
+        comb_iter = combinations(possible, i)
+        str = [''.join(citer) for citer in permutations(possible,i)] 
+        print(str)
 
 
 
@@ -10,3 +21,4 @@ import hashlib
 
 if __name__ == "__main__":
     #main function
+    combo()
