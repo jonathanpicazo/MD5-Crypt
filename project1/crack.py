1cf864441d85912e541b26dc739c14994891964b
jonathanpicazo <43653582+jonathanpicazo@users.noreply.github.com>
2021-01-13 18:32:57 -0800

    updated combinations to work with repeated characters

--- project1/crack.py
+++ project1/crack.py
@@ -1,24 +1,31 @@
 #team20:$1$4fTgjp6q$QhqtcnMK1gPnk0uN3CsyL/:16653:0:99999:7:::
 from itertools import combinations
-from itertools import permutations
 import hashlib
 import string
 
-#TODO: start combinations
 
-def combo():
+def getPermutations():
+
+    desired = input('Enter desired string to find with combo: ')
+    sz = len(desired)
+    # returns a list of all the possible permutations with lowercase letters that are less than or equal to 6 characters
     possible = string.ascii_lowercase
-    #print(possible)
-    for i in range(1,6):
-        comb_iter = combinations(possible, i)
-        str = [''.join(citer) for citer in permutations(possible,i)] 
-        print(str)
-
-
+    comb_str =[]
+    for i in range(1,int(sz) + 1):
+        for citer in combinations(possible,i):
+            temp = ''.join(citer)
+            if desired == temp:
+                print('found: ' + desired)
+                return
+            comb_str.append(temp)
+            print(temp)
+        possible += string.ascii_lowercase
+    print('did not find wanted string')
+    
 
 
 
 
 if __name__ == "__main__":
     #main function
-    combo()
+    getPermutations()
