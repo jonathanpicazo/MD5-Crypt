9c823be91a7ca6d7ad7ffe44984b9291886f8615
jonathanpicazo <43653582+jonathanpicazo@users.noreply.github.com>
2021-01-16 17:12:23 -0800

    just need to do final touches

--- project1/crack.py
+++ project1/crack.py
@@ -1,5 +1,5 @@
 #team20:$1$4fTgjp6q$QhqtcnMK1gPnk0uN3CsyL/:16653:0:99999:7:::
-from itertools import combinations
+from itertools import product
 import hashlib
 import string
 
@@ -9,40 +9,37 @@ gsalt = 'hfT7jp2q'
 def to64(v, n):
     base64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     ret = ''
-    for i in range(1, n):
+    for i in range(1, n + 1):
         ret += base64[v & 0x3f]
         v >>= 6
     return ret
 
-#TODO: finalization function
-
 def md5crypt():
     global gpassword, gsalt
-    password = gpassword.encode("ascii")
-    salt = gsalt.encode("ascii")
-    magic = "$1$".encode("ascii")
+    password = gpassword.encode('ascii')
+    salt = gsalt.encode('ascii')
+    magic = '$1$'.encode('ascii')
     res = password + magic + salt
     h = hashlib.md5(password + salt + password).digest()
     l = len(password)
-    print(h)
 
     while l > 0:
-        # res = res + substr(h, 0, min(16, l))  
         res += h[0:min(16,l)]
         l -= 16
     
     i = len(password)
     while i != 0:
-        if i & 1 == 0:
+        if i & 1:
             res += chr(0).encode("ascii")
         else:
             res += password[0:1]
         i >>= 1
     
     h = hashlib.md5(res).digest()
-    print(h)
-    for i in range(0,999):
-        tmp = b""
+    i = 0
+    # setting range from 999 to 1000 fixed the matching hash issue
+    for i in range(0,1000):
+        tmp = b''
         if i % 2 != 0:
             tmp += password
         else:
@@ -55,32 +52,26 @@ def md5crypt():
             tmp += h
         else:
             tmp += password
-    
-    abc = hashlib.md5(tmp).digest()
-
-    print(type(abc))
-
-    return to64((h[0] << 16) | (h[6] << 8) | (h[12]), 4) +to64((h[1] << 16) | (h[7] << 8) | (h[13]), 4) + to64((h[2] << 16) | (h[8] << 8) | (h[14]), 4) +to64((h[3] << 16) | (h[9] << 8) | (h[15]), 4) + to64((h[4] << 16) | (h[10] << 8) | (h[5]), 4) +to64(h[11], 2)
+        h = hashlib.md5(tmp).digest()
 
     
+    print('wPwz7GC6xLt9eQZ9eJkaq.')
+
+    answer = to64((h[0] << 16) | (h[6] << 8) | (h[12]), 4) +to64((h[1] << 16) | (h[7] << 8) | (h[13]), 4) + to64((h[2] << 16) | (h[8] << 8) | (h[14]), 4) +to64((h[3] << 16) | (h[9] << 8) | (h[15]), 4) + to64((h[4] << 16) | (h[10] << 8) | (h[5]), 4) +to64(h[11], 2)
+
+    print (answer)
 
 
 
 def getPermutations():
     desired = input('Enter desired string to find with combo: ')
-    sz = len(desired)
-    # returns a list of all the possible permutations with lowercase letters that are less than or equal to 6 characters
-    possible = string.ascii_lowercase + string.ascii_lowercase + string.ascii_lowercase + string.ascii_lowercase + string.ascii_lowercase + string.ascii_lowercase
-    comb_str = []
-    for citer in combinations(possible,6):
+    # returns a list of all the possible permutations with lowercase letters equal to 6 characters
+    # reverse ascii to make the cracking slightly faster, doubt the password will start with a letter that comes early in the alphabet
+
+    possible = string.ascii_lowercase[::-1]
+    for citer in product(possible,repeat = 6):
         temp = ''.join(citer)
-        if desired == temp:
-            print('found: ' + desired)
-            return
-        comb_str.append(temp)
         print(temp)
-    print('did not find wanted string')
-    
 
 
 
@@ -89,4 +80,5 @@ if __name__ == "__main__":
     #main function
     # passProvided = input("Input password\n")
     # saltProvided = input("Input salt\n")
+    #getPermutations()
     md5crypt()
